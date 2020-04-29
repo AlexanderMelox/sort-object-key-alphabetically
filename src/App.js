855f5a5ee0deed03969838dc8f369b48307a6d12
Alexander Melo <alexandermelox@gmail.com>
2020-04-29 17:24:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Global, css } from '@emotion/core'
+import styled from '@emotion/styled'
+import { motion } from 'framer-motion'
+import 'bulma/css/bulma.min.css'
+
+import { Header } from './components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Global
+        styles={css`
+          :root {
+            --color1: #d1faffff;
+            --color2: #9bd1e5ff;
+            --color3: #6a8eaeff;
+            --color4: #57a773ff;
+            --color5: #157145ff;
+          }
+
+          *,
+          *::before,
+          *::after {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+
+          ::selection {
+            color: white;
+            background: var(--color2);
+          }
+
+          body {
+            background-color: var(--color3);
+            color: var(--color1);
+            font-family: 'Open Sans', -apple-system, BlinkMacSystemFont,
+              'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+              'Helvetica Neue', sans-serif;
+            font-weight: 300;
+          }
+        `}
+      />
+      <StyledApp className="container is-fluid">
+        <motion.section
+          initial={{ opacity: 0, y: -200 }}
+          animate={{ opacity: 1, y: 0, rotate: -2 }}
+          whileHover={{ rotate: 0, scale: 1.1, color: 'white' }}
+          className="section"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Header>
+            Sort my object <br /> alphabetically.
+          </Header>
+        </motion.section>
+        <Lead
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.3 }}
+        >
+          Paste your Javascript object below and magic will happen{' '}
+          <span aria-label="magicians hat" role="img">
+            🧙🏼‍♂️
+          </span>
+        </Lead>
+        <TextareasContainer>
+          <Textarea className="box"></Textarea>
+          <Textarea className="box"></Textarea>
+        </TextareasContainer>
+      </StyledApp>
+    </>
+  )
 }
 
-export default App;
+const Textarea = styled.textarea`
+  background-color: #fff;
+  height: 500px;
+  width: 100%;
+  resize: none;
+  outline: none;
+  border: none;
+  border-radius: 0.3rem;
+  padding: 1rem;
+  font: inherit;
+`
+
+const TextareasContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1rem;
+`
+
+const Lead = styled(motion.p)`
+  text-align: center;
+  font-size: 1.3rem;
+  margin-bottom: 1rem;
+`
+
+const StyledApp = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+export const Header = styled.h1`
+  font-size: 5rem;
+  text-shadow: 6px 6px 0px rgba(0, 0, 0, 0.2);
+  font-family: 'Abril Fatface', cursive;
+  text-align: center;
+  line-height: 1.1;
+`
